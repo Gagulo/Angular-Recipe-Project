66d650ddf02f2cb45add5664792ac616db04f798
Gagulo <grzegorz.sokalski@hotmail.com>
2019-01-21 14:25:23 +0100

    signUp & singIn funcs added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCyrdPZxyjgUoahb77RhY5-U2NdEuvf5ZQ",
+      authDomain: "angular-recipe-app-71500.firebaseapp.com",
+    }) ;
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  signupUser(email: string, password: string) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(
+        error => console.log(error)
+      );
+  }
+
+  signInUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        response => console.log(response)
+      )
+      .catch(
+        error => console.log(error)
+      )
+  }
+}
