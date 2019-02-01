5d3a8dafea9307c2b9098507c206d96bf130063b
Gagulo <grzegorz.sokalski@hotmail.com>
2019-02-01 17:52:41 +0100

    deleting actions for recipe component

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
-import { Recipe } from '../recipe.model';
 import { RecipeService } from 'src/app/services/recipe.service';
 import * as ShoppingListActions from '../../reducers/shopping-list.actions';
-import * as fromApp from '../../reducers/app.reducers';
+import * as fromRecipe from '../../reducers/recipe.reducers';
+import * as RecipeActions from '../../reducers/recipe.actions';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -15,24 +16,28 @@ import * as fromApp from '../../reducers/app.reducers';
 export class RecipeDetailComponent implements OnInit {
 
   id: number;
-  recipe: Recipe;
+  recipeState: Observable<fromRecipe.State>;
 
   constructor(private recipeService: RecipeService,
               private route: ActivatedRoute,
               private router: Router,
-              private store: Store<fromApp.AppState>) { }
+              private store: Store<fromRecipe.FeatureState>) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
-        this.recipe = this.recipeService.getRecipe(this.id);
+        this.recipeState = this.store.select('recipes');
       }
     );
   }
 
   onAddToShoppingList() {
-    this.store.dispatch(new ShoppingListActions.AddIngredients(this.recipe.ingredients));
+    this.store.select('recipes')
+    .take(1)
+    .subscribe((recipeState: fromRecipe.State) => {
+    this.store.dispatch(new ShoppingListActions.AddIngredients(recipeState.recipes[this.id].ingredients));
+    });
   }
 
   onEditRecipe() {
@@ -40,7 +45,7 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   onDeleteRecipe() {
-    this.recipeService.deleteRecipe(this.id);
+    this.store.dispatch(new RecipeActions.DeleteRecipe(this.id));
     this.router.navigate(['/recipes']);
   }
 }
